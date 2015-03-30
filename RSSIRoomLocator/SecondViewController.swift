4d1e626228ad809f476d7c35ff66ac4fc03f908b
Tyler Casselman <tylerc230@gmail.com>
2015-03-29 20:31:59 -0700

    Initial Commit

---
+++ RSSIRoomLocator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RSSIRoomLocator
+//
+//  Created by Tyler Casselman on 3/29/15.
+//  Copyright (c) 2015 Casselman Consulting. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
