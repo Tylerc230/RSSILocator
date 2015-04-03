b94d54a06718815790ee3a3686bfc435cd67832a
Tyler Casselman <tylerc230@gmail.com>
2015-04-02 20:37:03 -0700

    Setup UI

---
+++ RSSIRoomLocator/RoomCountSelectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RoomCountSelectionViewController.swift
+//  RSSIRoomLocator
+//
+//  Created by Tyler Casselman on 4/2/15.
+//  Copyright (c) 2015 Casselman Consulting. All rights reserved.
+//
+
+import UIKit
+class RoomCountSelectionViewController: UIViewController {
+    @IBAction func dismissTapped() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
